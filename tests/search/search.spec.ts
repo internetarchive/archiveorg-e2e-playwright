c6bef8cdd3ef05561cc47edab9bfbe9886eae0b0
clau <1281581+dualcnhq@users.noreply.github.com>
2023-04-22 02:18:06 +0800

    Update base search test

--- tests/search/search.spec.ts
+++ tests/search/search.spec.ts
@@ -1,12 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('page load', async ({ page }) => {
   await page.goto('https://archive.org/search');
 
-  // Expects the URL to contain search.
   await expect(page).toHaveURL(/search/);
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Internet Archive: Digital Library of Free & Borrowable Books, Movies, Music & Wayback Machine/);
+  expect(await page.locator('#search-bar-container').count()).toEqual(1);
+  expect(await page.locator('#search_options').count()).toEqual(1);
+  expect(await page.locator('#action-bar-spacing').count()).toEqual(1);
+  expect(await page.getByRole('button', { name: 'GO GO' }).count()).toEqual(1);
+
+  expect(await page.locator('empty-placeholder').count()).toEqual(1);
+  expect(await page.getByRole('heading', { name: 'To begin searching, enter a search term in the box above and hit "Go".' }).count()).toEqual(1);
+  expect(await page.locator('#content-container svg').count()).toEqual(1);
+
+  const searchInput = page.getByRole('textbox', { name: 'Search the Archive. Filters and Advanced Search available below.' });
+
+  await searchInput.fill('cats');
+  await searchInput.press('Enter');
+
+  await page.waitForURL("https://archive.org/search?query=cats");
 });
 
