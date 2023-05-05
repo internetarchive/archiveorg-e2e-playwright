c49f66738e0cc4f307aa8cf9e2fff34fd789956a
clau <1281581+dualcnhq@users.noreply.github.com>
2023-05-05 08:05:06 +0800

    Add login tests

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -2,65 +2,104 @@ import { test, expect } from '@playwright/test';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-test('login page', async ({ page }) => {
-  // await page.goto('https://archive.org/account/login');
+test.beforeAll(async ({ page }) => {
+  // Go to the starting url before each test.
+  await page.goto('https://archive.org/account/login');
+  await expect(page).toHaveURL(/login/);
 
-  // await expect(page).toHaveURL(/login/);
+  const boxRow = page.locator('.box.row');
+  const loginHeader = boxRow.locator('.login-header');
+  const errorCookie = boxRow.locator('.error.cookie-warning.hidden');
+  const googleLogin = boxRow.locator('.third-party-sso');
 
-  // const boxRow = page.locator('.box.row');
-  // expect(await boxRow.count()).toEqual(1);
-
-  // const loginHeader = boxRow.locator('.login-header');
-  // expect(await loginHeader.count()).toEqual(1);
-
-  // const errorCookie = boxRow.locator('.error.cookie-warning.hidden');
-  // expect(await errorCookie.count()).toEqual(1);
-
-  // const googleLogin = boxRow.locator('.third-party-sso')
-  // expect(await googleLogin.count()).toEqual(1);
-
-  // const loginFormElement = boxRow.locator('.login-form-element');
-  // expect(await loginFormElement.count()).toEqual(1);
-
-  // const formLoginFields = loginFormElement.locator('.iaform.login-form');
-  // expect(await formLoginFields.count()).toEqual(1);
-
-  // const inputEmail = loginFormElement.locator('.form-element.input-email');
-  // expect(await inputEmail.count()).toEqual(1);
-
-  // const inputPassword = loginFormElement.locator('.form-element.input-password');
-  // expect(await inputPassword.count()).toEqual(1);
-
-  // const btnLogin = loginFormElement.locator('.btn.btn-primary.btn-submit.input-submit.js-submit-login')
-  // expect(await btnLogin.count()).toEqual(1);
-
-  // // fill-up form - with admin privs
-  // await inputEmail.click();
-  // await inputEmail.fill(process.env.EMAIL || '');
-  // await inputPassword.click();
-  // await inputPassword.fill(process.env.PASSWORD || '');
-  // await btnLogin.click();
-  
-  // await page.waitForURL('https://archive.org/');
-  await page.goto('https://archive.org/details/bdrc-W1PD137788?transpiled=1');
-
-  const iaBookTheater = page.locator('ia-book-theater');
-  const iaBookWrapper = iaBookTheater.locator('#IABookReaderMessageWrapper');
-  const iaLendingWrapper = iaBookTheater.locator('.lending-wrapper')
-  // const iaBookActions = iaBookWrapper.locator('ia-book-actions.focus-on-child-only');
-  expect(await iaBookTheater.count()).toEqual(1);
-  expect(await iaBookWrapper.count()).toEqual(1);
-  expect(await iaLendingWrapper.count()).toEqual(1);
-
-  const collapsibleActionGroup = iaBookWrapper.locator('collapsible-action-group');
-  expect(await collapsibleActionGroup.count()).toEqual(1);
-
-  const actionGroup = collapsibleActionGroup.locator(' actiongroup ');
-  const actionGroupPrimary = collapsibleActionGroup.locator('.action-buttons.primary');
-  const actionGroupSecondary = collapsibleActionGroup.locator('.action-buttons.secondary');
-  expect(await actionGroup.count()).toEqual(1);
-  expect(await actionGroupPrimary.count()).toEqual(1);
-  expect(await actionGroupSecondary.count()).toEqual(1);
-
+  expect(await boxRow.count()).toEqual(1);
+  expect(await loginHeader.count()).toEqual(1);
+  expect(await errorCookie.count()).toEqual(1);
+  expect(await googleLogin.count()).toEqual(1);
 });
 
+test('login page with user privs', async ({ page }) => {
+  const boxRow = page.locator('.box.row');
+  const loginFormElement = boxRow.locator('.login-form-element');
+  const formLoginFields = loginFormElement.locator('.iaform.login-form');
+  const inputEmail = loginFormElement.locator('.form-element.input-email');
+  const inputPassword = loginFormElement.locator('.form-element.input-password');
+  const btnLogin = loginFormElement.locator('.btn.btn-primary.btn-submit.input-submit.js-submit-login');
+
+  expect(await loginFormElement.count()).toEqual(1);
+  expect(await inputEmail.count()).toEqual(1);
+  expect(await formLoginFields.count()).toEqual(1);
+  expect(await inputPassword.count()).toEqual(1);
+  expect(await btnLogin.count()).toEqual(1);
+
+  // fill-up form - with admin privs
+  await inputEmail.click();
+  await inputEmail.fill(process.env.A_EMAIL || '');
+  await inputPassword.click();
+  await inputPassword.fill(process.env.A_PASSWORD || '');
+  await btnLogin.click();
+  
+  await page.waitForURL('https://archive.org/');
+  await page.goto('https://archive.org/details/bdrc-W1PD137788?transpiled=1');
+  await page.waitForTimeout(2000);
+
+  const iaBookTheater = page.locator('ia-book-theater.focus-on-child-only');
+  const iaBookWrapper = iaBookTheater.locator('#IABookReaderMessageWrapper');
+  const iaBookActions = iaBookWrapper.locator('ia-book-actions');
+
+  expect(await iaBookTheater.count()).toEqual(1);
+  expect(await iaBookWrapper.count()).toEqual(1);
+  expect(await iaBookActions.count()).toEqual(1);
+
+  const collapsibleActionGroup = iaBookActions.locator('collapsible-action-group');
+  expect(await collapsibleActionGroup.count()).toEqual(1);
+
+  const btnBorrow = collapsibleActionGroup.getByRole('button', { name: 'Borrow for 1 hour' });;
+  const btnAdminAccess = collapsibleActionGroup.getByRole('link', { name: 'Admin Access' });;
+
+  expect(await btnBorrow.count()).toEqual(1);
+  expect(await btnAdminAccess.count()).toEqual(1);
+});
+
+test('login page with regular user privs', async ({ page }) => {
+  const boxRow = page.locator('.box.row');
+  const loginFormElement = boxRow.locator('.login-form-element');
+  const formLoginFields = loginFormElement.locator('.iaform.login-form');
+  const inputEmail = loginFormElement.locator('.form-element.input-email');
+  const inputPassword = loginFormElement.locator('.form-element.input-password');
+  const btnLogin = loginFormElement.locator('.btn.btn-primary.btn-submit.input-submit.js-submit-login');
+
+  expect(await loginFormElement.count()).toEqual(1);
+  expect(await inputEmail.count()).toEqual(1);
+  expect(await formLoginFields.count()).toEqual(1);
+  expect(await inputPassword.count()).toEqual(1);
+  expect(await btnLogin.count()).toEqual(1);
+
+  // fill-up form
+  await inputEmail.click();
+  await inputEmail.fill(process.env.B_EMAIL || '');
+  await inputPassword.click();
+  await inputPassword.fill(process.env.B_PASSWORD || '');
+  await btnLogin.click();
+  
+  await page.waitForURL('https://archive.org/');
+  await page.goto('https://archive.org/details/bdrc-W1PD137788?transpiled=1');
+  await page.waitForTimeout(2500);
+
+  const iaBookTheater = page.locator('ia-book-theater.focus-on-child-only');
+  const iaBookWrapper = iaBookTheater.locator('#IABookReaderMessageWrapper');
+  const iaBookActions = iaBookWrapper.locator('ia-book-actions');
+
+  expect(await iaBookTheater.count()).toEqual(1);
+  expect(await iaBookWrapper.count()).toEqual(1);
+  expect(await iaBookActions.count()).toEqual(1);
+
+  const collapsibleActionGroup = iaBookActions.locator('collapsible-action-group');
+  expect(await collapsibleActionGroup.count()).toEqual(1);
+
+  const btnBorrow = collapsibleActionGroup.getByRole('button', { name: 'Borrow for 1 hour' });;
+  const btnAdminAccess = collapsibleActionGroup.getByRole('link', { name: 'Admin Access' });;
+
+  expect(await btnBorrow.count()).toEqual(1);
+  expect(await btnAdminAccess.count()).toEqual(0);
+});
