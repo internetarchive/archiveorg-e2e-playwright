f4c4134a4a8e7859c91fe81adb547bede3d569fe
clau <1281581+dualcnhq@users.noreply.github.com>
2023-06-07 02:32:12 +0800

    Disable browserstack for GH action debugging runtime flow

--- tests/search/search-view-mode.spec.ts
+++ tests/search/search-view-mode.spec.ts
@@ -3,22 +3,24 @@ import { test, expect } from '@playwright/test';
 test('page load', async ({ page }) => {
   await page.goto('https://archive.org/search?query=cats');
 
-  await expect(page).toHaveURL(/?query=cats/);
+  await expect(page).toHaveURL(/query=cats/);
 
-  expect(await page.locator('#search-bar-container').count()).toEqual(1);
-  expect(await page.locator('#search_options').count()).toEqual(1);
-  expect(await page.locator('#action-bar-spacing').count()).toEqual(1);
-  expect(await page.getByRole('button', { name: 'GO GO' }).count()).toEqual(1);
+  const sortFilterBar = page.locator('sort-filter-bar');
+  const sortContainer = sortFilterBar.locator('#container');
+  const sortBarSection = sortFilterBar.locator('section#sort-bar');
 
-  expect(await page.locator('empty-placeholder').count()).toEqual(1);
-  expect(await page.getByRole('heading', { name: 'To begin searching, enter a search term in the box above and hit "Go".' }).count()).toEqual(1);
-  expect(await page.locator('#content-container svg').count()).toEqual(1);
+  const sortDirectionSelector = sortBarSection.locator('.sort-direction-container');
+  const sortByText = sortBarSection.locator('.sort-by-text');
+  const sortSelectorContainer = sortBarSection.locator('div#sort-selector-container');
+  const displayStyleSelector = sortBarSection.locator('div#display-style-selector');
 
-  const searchInput = page.getByRole('textbox', { name: 'Search the Archive. Filters and Advanced Search available below.' });
+  expect(await sortFilterBar.count()).toEqual(1);
+  expect(await sortContainer.count()).toEqual(1);
+  expect(await sortBarSection.count()).toEqual(1);
 
-  await searchInput.fill('cats');
-  await searchInput.press('Enter');
-
-  await page.waitForURL("https://archive.org/search?query=cats");
+  expect(await sortDirectionSelector.count()).toEqual(1);
+  expect(await sortByText.count()).toEqual(1);
+  expect(await sortSelectorContainer.count()).toEqual(1);
+  expect(await displayStyleSelector.count()).toEqual(1);
+  
 });
-
