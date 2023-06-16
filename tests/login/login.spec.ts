35ab532af991d05c29577a9e164ea25dd9c8bfca
clau <1281581+dualcnhq@users.noreply.github.com>
2023-06-17 01:57:03 +0800

    Add caching and change login test spec

---
+++ tests/login/login.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('login page fields', async ({ page }) => {
+  // Go to the starting url before each test.
+  await page.goto('https://archive.org/account/login');
+  await expect(page).toHaveURL(/login/);
+
+  const boxRow = page.locator('.box.row');
+  const loginFormElement = boxRow.locator('.login-form-element');
+  const formLoginFields = loginFormElement.locator('.iaform.login-form');
+  const inputEmail = loginFormElement.locator('.form-element.input-email');
+  const inputPassword = loginFormElement.locator('.form-element.input-password');
+  const btnLogin = loginFormElement.locator('.btn.btn-primary.btn-submit.input-submit.js-submit-login');
+
+  expect(await loginFormElement.count()).toEqual(1);
+  expect(await inputEmail.count()).toEqual(1);
+  expect(await formLoginFields.count()).toEqual(1);
+  expect(await inputPassword.count()).toEqual(1);
+  expect(await btnLogin.count()).toEqual(1);
+});
