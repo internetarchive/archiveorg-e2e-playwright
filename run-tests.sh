7a2aa55701dab160b0e18ecaf89b86977776aa52
clau <1281581+dualcnhq@users.noreply.github.com>
2024-02-17 16:36:24 +0800

    update scripts - for now

--- run-tests.sh
+++ run-tests.sh
@@ -1,11 +1,8 @@
-#!/bin/sh
-
-npm run test:books
-
-npm run test:collection
-
-npm run test:login
-
-npm run test:music
-
-npm run test:search
+TIMEFORMAT='It took %0R seconds.' 
+time {
+  npm run test:books
+  npm run test:login
+  npm run test:music
+  npm run test:search
+  # npm run test:collection
+}
