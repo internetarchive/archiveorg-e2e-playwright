8ad3acecb473cc51776f0027b0f61503f72f1c97
clau <1281581+dualcnhq@users.noreply.github.com>
2024-03-07 18:45:34 +0800

    update music page tests

--- tests/music/music.spec.ts
+++ tests/music/music.spec.ts
@@ -2,18 +2,26 @@ import { test } from '../fixtures';
 
 test('Load and Play a Grateful Dead Soundtrack', async ({ musicPage }) => {
   await musicPage.gotoMusicPage('gd73-06-10.sbd.hollister.174.sbeok.shnf');
-  await musicPage.validateCommonPageElements();
+  await musicPage.validateMusicPageElements();
+  // await musicPage.interactWithBookReader();
   await musicPage.playAndPauseMusic();
 });
 
-test(`Special case: Audio item without image - with waveform`, async ({ musicPage }) => {
+test(`Special case: Audio item without image - with waveform`, async ({
+  musicPage,
+}) => {
   await musicPage.gotoMusicPage('gd77-05-08.sbd.hicks.4982.sbeok.shnf');
+  await musicPage.validateMusicPageElements();
   await musicPage.validateNoImageAvailableInPhotoViewerDisplay(true);
+  // await musicPage.interactWithBookReader();
   await musicPage.validateNoWaveformDisplay(false);
 });
 
-test(`Special case: Load a single track - no waveform`, async ({ musicPage }) => {
+test(`Special case: Load a single track - no waveform`, async ({
+  musicPage,
+}) => {
   await musicPage.gotoMusicPage('berceuse00benj');
+  await musicPage.validateMusicPageElements();
   await musicPage.validateNoImageAvailableInPhotoViewerDisplay(false);
   await musicPage.validateNoWaveformDisplay(true);
 });
--- tests/page-objects/music-page.ts
+++ tests/page-objects/music-page.ts
@@ -1,16 +1,29 @@
 import { type Page, type Locator, expect } from '@playwright/test';
 
+import { BookPage } from './book-page';
 export class MusicPage {
   readonly page: Page;
+
   readonly iauxPhotoViewer: Locator;
   readonly iauxMusicTheater: Locator;
   readonly playAv: Locator;
+  readonly seeMoreCta: Locator;
+  readonly brSlot: Locator;
+  readonly bookReaderShell: Locator;
+
+  readonly bookPage: BookPage;
 
   public constructor(page: Page) {
     this.page = page;
+
     this.iauxMusicTheater = this.page.locator('ia-music-theater');
-    this.iauxPhotoViewer = this.page.locator('#music-theater > iaux-photo-viewer');
+    this.iauxPhotoViewer = this.page.locator(
+      '#music-theater > iaux-photo-viewer',
+    );
     this.playAv = this.page.locator('play-av');
+    this.seeMoreCta = this.iauxPhotoViewer.locator('#see-more-cta');
+    this.brSlot = this.page.locator('#theatre-ia > div.row > div > ia-music-theater > div.bookreader-slot');
+    this.bookReaderShell = this.brSlot.locator('#BookReader');
   }
 
   async gotoMusicPage(uri: string) {
@@ -21,7 +34,7 @@ export class MusicPage {
     await this.page.waitForTimeout(5000);
   }
 
-  async validateCommonPageElements() {
+  async validateMusicPageElements() {
     // player controls
     const musicTheater = this.iauxMusicTheater.locator('#music-theater');
     const channelSelector = this.iauxMusicTheater.locator('channel-selector');
@@ -34,26 +47,81 @@ export class MusicPage {
 
     await expect(this.iauxPhotoViewer).toBeVisible({ timeout: 60000 });
     await expect(this.playAv).toBeVisible({ timeout: 60000 });
+
+    await expect(
+      this.playAv.locator('div.playlist > div.track-list'),
+    ).toBeVisible({ timeout: 60000 });
   }
 
-  async validateNoImageAvailableInPhotoViewerDisplay(noImage: boolean) {
-    if (noImage) {
-      expect(await this.iauxPhotoViewer.getAttribute('noimageavailable')).toBe('')
-      await expect(this.iauxPhotoViewer.locator('iamusic-noimage')).toBeVisible({timeout: 60000});
-      await expect(this.iauxMusicTheater.locator('#BookReader')).not.toBeVisible({timeout: 60000});
+  async validateNoImageAvailableInPhotoViewerDisplay(noPlaceholder: boolean) {
+    console.log('noPlaceholder: ', noPlaceholder)
+    
+    if (noPlaceholder) {
+      expect(await this.iauxPhotoViewer.getAttribute('noimageavailable')).toBe(
+        '',
+      );
+      await expect(this.iauxPhotoViewer.locator('iamusic-noimage')).toBeVisible(
+        { timeout: 60000 },
+      );
+      await expect(
+        this.iauxMusicTheater.locator('#BookReader'),
+      ).not.toBeVisible({ timeout: 60000 });
     } else {
-      expect(await this.iauxPhotoViewer.getAttribute('noimageavailable')).toBe(null);
-      await expect(this.iauxPhotoViewer.locator('#see-more-cta')).toBeVisible({timeout: 60000});
-      await expect(this.iauxMusicTheater.locator('#BookReader')).toBeVisible({timeout: 60000});
+      expect(await this.iauxPhotoViewer.getAttribute('noimageavailable')).toBe(
+        null,
+      );
+      await expect(this.seeMoreCta).toBeVisible({
+        timeout: 60000,
+      });
+      await expect(this.iauxMusicTheater.locator('#BookReader')).toBeVisible({
+        timeout: 60000,
+      });
+
+      await this.interactWithBookReader();
     }
+  }
+
+  async getBookReaderClass() {
+    return await this.bookReaderShell.getAttribute('class', { timeout: 3000 });
+  }
+
+  async interactWithBookReader() {
+    console.log('interact with bookreader')
+
+    const oneUpClass = 'BRmode1up';
+    const thumbViewModeClass = 'BRmodeThumb';
+    const fullScreenClass = 'fullscreenActive';
+    const brFooter = this.bookReaderShell.locator('.BRfooter');
+
+    await this.seeMoreCta.click({ timeout: 30000 });
+    expect(await this.iauxPhotoViewer.getAttribute('showallphotos')).toBe('');
+
+    // initial load
+    expect(await this.getBookReaderClass()).toContain(oneUpClass);
+
+    // click fullscreen
+    await brFooter.locator('.BRicon.full').click();
+    expect(await this.getBookReaderClass()).toContain(fullScreenClass);
+
+    // click thumbnail mode
+    await brFooter.locator('.BRicon.thumb').click();
+    expect(await this.getBookReaderClass()).toContain(thumbViewModeClass);
+
+    // close BookReader photoViewer
+    await this.iauxPhotoViewer.locator('#close-photo-viewer').click();
+    expect(await this.iauxPhotoViewer.getAttribute('showallphotos')).toBe(null);
   }
 
   async validateNoWaveformDisplay(noWaveform: boolean) {
     const blackWaveform = 'https://archive.org/images/black.jpg';
-    if(noWaveform) {
-      expect(await this.playAv.locator('#waveformer-wrap > img').getAttribute('src')).toBe(blackWaveform)
+    if (noWaveform) {
+      expect(
+        await this.playAv.locator('#waveformer-wrap > img').getAttribute('src'),
+      ).toBe(blackWaveform);
     } else {
-      expect(await this.playAv.locator('#waveformer-wrap > img').getAttribute('src')).not.toBe(blackWaveform)
+      expect(
+        await this.playAv.locator('#waveformer-wrap > img').getAttribute('src'),
+      ).not.toBe(blackWaveform);
     }
   }
 
@@ -69,5 +137,4 @@ export class MusicPage {
       this.page.locator('#jw6.jwplayer.jw-reset.jw-state-paused'),
     ).toBeVisible({ timeout: 60000 });
   }
-
 }
