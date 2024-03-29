117cba55dae5e7cb3887da89cba90fd7ff0736cd
clau <1281581+dualcnhq@users.noreply.github.com>
2024-03-29 12:43:11 +0800

    update test description

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -1,17 +1,17 @@
 import { test } from '../fixtures';
 
-test('Login as a patron', async ({ patronLoginPage }) => {
+test('Account settings - Login as a patron', async ({ patronLoginPage }) => {
   await test.step('Check account settings page after logging in, and verify Account Settings page displayed', async () => {
     await patronLoginPage.assertAccountSettingsDisplayed();
   });
 });
 
-test('Login as a admin', async ({ privsLoginPage }) => {
+test('Account settings - Login as a admin', async ({ privsLoginPage }) => {
   await test.step('Check account settings page after logging in, and verify Account Settings page displayed', async () => {
     await privsLoginPage.assertAccountSettingsDisplayed();
   });
 });
 
-test('Not logged in', async ({ loginPage }) => {
+test('Account settings - Not logged in', async ({ loginPage }) => {
   await loginPage.notLoggedIn();
 });
