35bcf845525ebf281c9c46188d8a41f5528cfc61
Neeraj sharma <neeraj@archive.org>
2024-07-03 21:02:06 +0530

    Auto Renew Timer Tests

--- tests/details/lending-bar.spec.ts
+++ tests/details/lending-bar.spec.ts
@@ -20,14 +20,35 @@ import { test } from '../fixtures';
 /**
  * auto-renew feature testing starts here.......
  */
-test(`Lending-Bar with auto-renew feature...`, async ({ lendingBar }) => {
-  await test.step('without logged-in', async () => {
-    const demoApp = 'https://internetarchive.github.io/iaux-book-actions/pr/pr-72/';
+test(`Lending-Bar with auto-renew for 5 minutes`, async ({ lendingBar }) => {
+  const demoApp = 'https://internetarchive.github.io/iaux-book-actions/pr/pr-73/?timer=5';
+
+  await test.step('user browsed for 5min', async () => {
     await lendingBar.gotoPage(demoApp);
+    await lendingBar.autoRenew_userJustBrowsedFor5Min();
+  });
 
-    // await lendingBar.autoRenew_verifyDefaultTexts();
+  await test.step('user browsed for 5min - renewed', async () => {
+    await lendingBar.gotoPage(demoApp);
+    await lendingBar.autoRenew_userJustBrowsedFor5MinRenewed();
+  });
+});
 
-    await lendingBar.autoRenew_userJustBrowsed();
-    // await lendingBar.autoRenew_userHasBrowsed();
+test(`Lending-Bar with auto-renew for 10 minutes`, async ({ lendingBar }) => {
+  const demoApp = 'https://internetarchive.github.io/iaux-book-actions/pr/pr-73/?timer=10';
+
+  await test.step('user browsed for 10min', async () => {
+    await lendingBar.gotoPage(demoApp);
+    await lendingBar.autoRenew_userJustBrowsedFor10Min();
+  });
+
+  await test.step('user browsed for 10min - renewed', async () => {
+    await lendingBar.gotoPage(demoApp);
+    await lendingBar.autoRenew_userJustBrowsedFor10MinRenewed();
+  });
+
+  await test.step('user make activity to get silent renewed', async () => {
+    await lendingBar.gotoPage(demoApp);
+    await lendingBar.autoRenew_autoRenewWhenUserMakeActivity();
   });
 });
--- tests/page-objects/lending-bar.ts
+++ tests/page-objects/lending-bar.ts
@@ -1,8 +1,4 @@
 import { type Page, Locator, expect } from '@playwright/test';
-// import clock
-// import clock
-// import { createClock as rawCreateClock, install as rawInstall } from '../../packages/playwright-core/src/server/injected/clock';
-// import type { InstallConfig, ClockController, ClockMethods } from '../../packages/playwright-core/src/server/injected/clock';
 
 export class LendingBar {
   readonly page: Page;
@@ -10,58 +6,196 @@ export class LendingBar {
   readonly iaBookActions: Locator;
   readonly demoControls: Locator;
 
+  readonly loanSecondsFor5Min: Number;
+  readonly loanSecondsFor10Min: Number;
+  readonly loanSecondsFor30Min: Number;
+  readonly loanSecondsFor60Min: Number;
+
   public constructor(page: Page) {
     this.page = page;
 
     this.iaBookActions = this.page.locator('ia-book-actions');
     this.demoControls = this.page.locator('.initial');
+
+    this.loanSecondsFor5Min = 300; // seconds
+    this.loanSecondsFor10Min = 600; // seconds 
+    this.loanSecondsFor30Min = 1800; // seconds
+    this.loanSecondsFor60Min = 3600; // seconds
+  }
+
+  async clickOnBrowsedButton() {
+    const timeInBrowser = await this.page.evaluate(() => {
+      return new Date().toString();
+    });
+    console.log(timeInBrowser);
+
+    const browsedCheckbox = this.demoControls.getByText('user_has_browsed', { exact: true });
+    await browsedCheckbox.click();
   }
 
   async gotoPage(uri: string) {
     await this.page.goto(uri, { waitUntil: 'networkidle' });
-    await this.page.waitForTimeout(5000);
+
+    await this.page.clock.setSystemTime(new Date().toISOString());
+    await this.page.waitForTimeout(1000);
   }
 
-
-  // auto renew feature testing starts from here....
-  // auto renew feature testing starts from here....
-  // auto renew feature testing starts from here....
-  // auto renew feature testing starts from here....
-  // auto renew feature testing starts from here....
-  // auto renew feature testing starts from here....
-  // auto renew feature testing starts from here....
-  async autoRenew_verifyDefaultTexts() {
-    const textGroup = await this.iaBookActions.locator('text-group > .variable-texts');
-    const textGroupTexts = await textGroup.textContent();
-
-    await expect(textGroup).toBeVisible();
-    await expect(textGroupTexts).toContain('Renews automatically with continued use.');
+  async getTimerCountdownSeconds() {
+    const timerElement = this.page.locator('timer-countdown .second');
+    if (timerElement) return Number(await timerElement.textContent());
+    return false;
   }
 
-  async autoRenew_userJustBrowsed() {
-    const browsedCheckbox = this.demoControls.getByText('user_has_browsed', { exact: true });
-    await browsedCheckbox.click();
-  
-    // ERRRRRRRRRRRRRRRRRRRR
-    await this.page.clock.install({ time: new Date('2024-02-02T08:00:00') });
-    await this.page.clock.fastForward('01:00');
+  // auto renew feature testing starts from here....
+  /**
+   * 5 minutes loan tests, the timer details are as follow:-
+   * - loanTotalTime: 300,
+   * - loanRenewAtLast: 240,
+   * - pageChangedInLast: 60
+   */
+  async autoRenew_userJustBrowsedFor5Min() {
+    await this.clickOnBrowsedButton();
 
-    console.log(this.iaBookActions)
-    // wait for navigation to complete
-    await this.page.waitForTimeout(5000);
-    
+    // install clock timer is -- 300000ms / 300s / 5min
+    await this.page.clock.install(Number(this.loanSecondsFor5Min) * 1000);
+
+    // fast-forward timer by -- 01:10 / 70s
+    await this.page.clock.fastForward('01:20'); // 1m10s
+
+    // after 240sec, we expected to warning message having backdrop
     await expect(this.page.locator('.backdrop')).toBeVisible();
+    await expect(this.page.getByText('Are you still reading?')).toBeVisible();
+
+    // again fast-forward timer by -- 03:02 / 180s
+    await this.page.clock.fastForward('05:00');
+    await this.page.waitForTimeout(1000);
+
     await expect(this.page.getByText('This book has been returned')).toBeVisible();
   }
 
-  // auto renew feature testing ends from here....
-  // auto renew feature testing ends from here....
-  // auto renew feature testing ends from here....
-  // auto renew feature testing ends from here....
-  // auto renew feature testing ends from here....
-  // auto renew feature testing ends from here....
-  // auto renew feature testing ends from here....
+  async autoRenew_userJustBrowsedFor5MinRenewed() {
+    await this.clickOnBrowsedButton();
 
+    const totalLoanSeconds = 300;
+
+    // install clock timer is -- 300000ms / 300s / 5min
+    await this.page.clock.install(Number(this.loanSecondsFor5Min) * 1000);
+
+    // fast-forward timer by -- 01:10 / 70s
+    await this.page.clock.fastForward('01:20'); // 1m10s
+
+    // after 240sec, we expected to warning message having backdrop
+    await expect(this.page.locator('.backdrop')).toBeVisible();
+    await expect(this.page.getByText('Are you still reading?')).toBeVisible();
+
+    await expect(this.page.locator('#book-action-bar-custom-buttons')).toBeVisible();
+
+    const keepReadingButton = this.page.locator('#book-action-bar-custom-buttons button').first();
+    if (keepReadingButton) {
+      await keepReadingButton.click();
+      await this.page.waitForTimeout(1000);
+    }
+
+    await expect(this.page.locator('.backdrop')).not.toBeVisible();
+
+    // timer has been reset and match with total loan seconds... :)
+    expect(await this.getTimerCountdownSeconds()).toEqual(this.loanSecondsFor5Min);
+
+    // again fast-forward timer by -- 03:02 / 180s
+    await this.page.clock.fastForward('02:00');
+    await this.page.waitForTimeout(1000);
+
+    await expect(this.page.locator('.backdrop')).toBeVisible();
+  }
+
+  /**
+   * 10 minutes loan tests, the timer details are as follow:-
+   * - loanTotalTime: 600,
+   * - loanRenewAtLast: 480,
+   * - pageChangedInLast: 120
+   */
+  async autoRenew_userJustBrowsedFor10Min() {
+    await this.clickOnBrowsedButton();
+
+    // install clock timer is -- 600000ms / 600s / 10min
+
+    await this.page.clock.install(Number(this.loanSecondsFor10Min) * 1000);
+
+    // fast-forward timer by -- 01:10 / 70s
+    await this.page.clock.fastForward('02:20'); // 1m10s
+
+    // after 240sec, we expected to warning message having backdrop
+    await expect(this.page.locator('.backdrop')).toBeVisible();
+    await expect(this.page.getByText('Are you still reading?')).toBeVisible();
+
+    // // again fast-forward timer by -- 03:02 / 180s
+    await this.page.clock.fastForward('08:00');
+    await this.page.waitForTimeout(1000);
+
+    await expect(this.page.getByText('This book has been returned')).toBeVisible();
+    await this.page.waitForTimeout(1000);
+  }
+
+  async autoRenew_userJustBrowsedFor10MinRenewed() {
+    await this.clickOnBrowsedButton();
+
+    await this.page.clock.install(Number(this.loanSecondsFor10Min) * 1000);
+
+    // fast-forward timer by -- 01:10 / 70s
+    await this.page.clock.fastForward('02:20'); // 1m10s
+
+    // after 240sec, we expected to warning message having backdrop
+    await expect(this.page.locator('.backdrop')).toBeVisible();
+    await expect(this.page.getByText('Are you still reading?')).toBeVisible();
+
+    await expect(this.page.locator('#book-action-bar-custom-buttons')).toBeVisible();
+
+    const keepReadingButton = this.page.locator('#book-action-bar-custom-buttons button').first();
+    if (keepReadingButton) {
+      await keepReadingButton.click();
+      await this.page.waitForTimeout(1000);
+    }
+
+    await expect(this.page.locator('.backdrop')).not.toBeVisible();
+
+    // timer has been reset and match with total loan seconds... :)
+    expect(await this.getTimerCountdownSeconds()).toEqual(this.loanSecondsFor10Min);
+
+    // again fast-forward timer by -- 03:02 / 180s
+    await this.page.clock.fastForward('05:00');
+    await this.page.waitForTimeout(1000);
+
+    await expect(this.page.getByText('This book has been returned')).not.toBeVisible();
+  }
+
+  async autoRenew_autoRenewWhenUserMakeActivity() {
+    await this.clickOnBrowsedButton();
+    await this.page.clock.install(Number(this.loanSecondsFor10Min) * 1000);
+
+    // fast-forward timer by -- 01:10 / 70s
+    await this.page.clock.fastForward('01:50'); // 1m10s
+
+    // user made activity on BookReader
+    const pageChangedElement = await this.page.locator('.pageChangedEvent');
+    if (pageChangedElement) {
+      await pageChangedElement.click();
+      await this.page.clock.fastForward('01:55'); // 1m10s
+      await this.page.waitForTimeout(5000);
+    }
+
+    // timer has been reset and match with total loan seconds... :)
+    expect(await this.getTimerCountdownSeconds()).toEqual(this.loanSecondsFor10Min);
+
+    await expect(this.page.locator('.backdrop')).not.toBeVisible();
+    await expect(this.page.getByText('Are you still reading?')).not.toBeVisible();
+  }
+  // auto renew feature testing ends from here....
+  // auto renew feature testing ends from here....
+  // auto renew feature testing ends from here....
+  // auto renew feature testing ends from here....
+  // auto renew feature testing ends from here....
+  // auto renew feature testing ends from here....
+  // auto renew feature testing ends from here....
 
   async verifyDefaultTexts() {
     const textGroup = await this.iaBookActions.locator('text-group > .variable-texts');
