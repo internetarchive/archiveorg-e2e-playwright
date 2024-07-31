1591f816bd29cb477da102e45adc92ba4903787a
clau <1281581+dualcnhq@users.noreply.github.com>
2024-08-01 01:55:37 +0800

    WEBDEV-6884 update scripts execution (#50)

---
+++ scripts/generateCommand.js
@@ -0,0 +1,47 @@
+const generateCommand = (args) => {
+  // Format the arguments into a command string
+  const commandArgs = Object.entries(args)
+    .filter(([_, value]) => {
+      // Remove blank arguments
+      if (value === '' || value === undefined || value === false) {
+        return false;
+      }
+      return true;
+    })
+    .map(([key, value]) => {
+      if (typeof value === 'boolean') {
+        return value ? `--${key}` : '';
+      } else {
+        // If the argument is a title, surround it with ""
+        if (key === 'title') {
+          return `--${key}="${value}"`;
+        } else {
+          return `--${key}=${value}`;
+        }
+      }
+    })
+    .join(' '); // Join the arguments with a space
+
+  // Return the formatted command
+  return `npm run test -- ${commandArgs}`;
+};
+
+/**
+ * Test category options: 'about', 'av', 'books', 'collection', 'details', 'donation', 'home', 'login', 'music', 'profile', 'search'
+ * Browser options: 'chromium', 'firefox', 'webkit'
+ * Device options: 'mobile', 'desktop'
+ * Title: '<test case title description; see *.spec.ts files to get the titles>'
+ */
+const sampleArgs = {
+  test: 'books',
+  browser: 'chromium', // default/blank: all browsers
+  title: '',
+  headed: false,
+  trace: false,
+  debug: false,
+  ui: true,
+  device: '' // default/blank: desktop
+};
+
+const sampleCommand = generateCommand(sampleArgs);
+console.log('Generated command:', sampleCommand);
