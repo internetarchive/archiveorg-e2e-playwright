4b86249129e1233b0a75222645e728c158dcfc1a
clau <1281581+dualcnhq@users.noreply.github.com>
2025-01-08 03:58:38 +0800

    update locators in about pages

--- tests/about/about.spec.ts
+++ tests/about/about.spec.ts
@@ -8,23 +8,24 @@ test.beforeEach(async ({ context }) => {
 
 test('Canonical About page has correct title and text', async ({ page }) => {
   await page.goto(identifier.about.url);
-  await expect(page).toHaveTitle(/About IA/);
+  await expect(page).toHaveTitle(/Internet Archive: Digital Library/);
   await expect(page.locator('h1:has-text("About the Internet Archive")')).toBeVisible();
-  await expect(page.locator('a:has-text("Terms of Service")')).toBeVisible();
-  await expect(page.locator('a:has-text("Job Opportunities")')).toBeVisible();
+  await expect(page.locator('#maincontent').locator('a:has-text("Job Opportunities")')).toBeVisible();
+  await expect(page.locator('#maincontent').locator('a:has-text("Terms of Service")')).toBeVisible();
   await expect(page.locator('#maincontent')).toContainText('501(c)(3) non-profit');
+  await expect(page.locator('news-stories')).toBeVisible();
 });
 
 test('About > Jobs page has correct title and text', async ({ page }) => {
   await page.goto(identifier.about.jobs);
-  await expect(page).toHaveTitle(/Jobs/);
+  await expect(page).toHaveTitle(/Internet Archive: Digital Library/);
   await expect(page.locator('h1:has-text("Job Opportunities")')).toBeVisible();
   await expect(page.locator('#maincontent')).toContainText('Based in San Francisco');
 });
 
 test('About > Terms of Service page has correct title and text', async ({page}) => {
   await page.goto(identifier.about.terms);
-  await expect(page).toHaveTitle(/Terms of Use/);
+  await expect(page).toHaveTitle(/Internet Archive: Digital Library/);
   await expect(page.locator('h1:has-text("Terms of Use")')).toBeVisible();
   await expect(page.locator('#maincontent')).toContainText(
     'This terms of use agreement',
