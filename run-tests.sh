e2204ee19861469c678620f26ef782da3fd32ed5
clau <1281581+dualcnhq@users.noreply.github.com>
2025-01-15 02:48:56 +0800

    update script and gh workflow

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,10 @@
 TIMEFORMAT='It took %0R seconds.' 
 time {
-  npm run test
+  npx browserstack-node-sdk playwright test ./tests/av
+  npx browserstack-node-sdk playwright test ./tests/books
+  npx browserstack-node-sdk playwright test ./tests/collection
+  npx browserstack-node-sdk playwright test ./tests/donation
+  npx browserstack-node-sdk playwright test ./tests/home
+  npx browserstack-node-sdk playwright test ./tests/login
+  npx browserstack-node-sdk playwright test ./tests/profile
 }
